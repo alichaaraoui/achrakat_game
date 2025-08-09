f89978de5e99c0cc42db98d1f2e158a78c2b3581
alichaaraoui <96386840+alichaaraoui@users.noreply.github.com>
2025-08-09 01:32:35 -0400

    Add StarterAssets (85MB)

---
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -0,0 +1,281 @@
+﻿using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+    [RequireComponent(typeof(CharacterController))]
+#if ENABLE_INPUT_SYSTEM
+    [RequireComponent(typeof(PlayerInput))]
+#endif
+    public class ThirdPersonController : MonoBehaviour
+    {
+        [Header("Player")]
+        public float MoveSpeed = 2.0f;
+        public float SprintSpeed = 5.335f;
+        [Range(0.0f, 0.3f)] public float RotationSmoothTime = 0.12f;
+        public float SpeedChangeRate = 10.0f;
+        public AudioClip LandingAudioClip;
+        public AudioClip[] FootstepAudioClips;
+        [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+
+        [Space(10)]
+        public float JumpHeight = 1.2f;
+        public float Gravity = -15.0f;
+
+        [Space(10)]
+        public float JumpTimeout = 0.50f;
+        public float FallTimeout = 0.15f;
+
+        [Header("Player Grounded")]
+        public bool Grounded = true;
+        public float GroundedOffset = -0.14f;
+        public float GroundedRadius = 0.28f;
+        public LayerMask GroundLayers;
+
+        [Header("Cinemachine")]
+        public GameObject CinemachineCameraTarget;
+        public float TopClamp = 70.0f;
+        public float BottomClamp = -30.0f;
+        public float CameraAngleOverride = 0.0f;
+        public bool LockCameraPosition = false;
+
+        [Header("Veil Effect")]
+        public TrailRenderer VeilEffect;
+        public float glideGravityMultiplier = 0.2f;
+        public float glideLiftForce = 8f;
+        public float glideCost = 20f;
+        public float veilCharge = 100f;
+        public float maxVeilCharge = 100f;
+
+        private float _cinemachineTargetYaw;
+        private float _cinemachineTargetPitch;
+        private float _speed;
+        private float _animationBlend;
+        private float _targetRotation = 0.0f;
+        private float _rotationVelocity;
+        private float _verticalVelocity;
+        private float _terminalVelocity = 53.0f;
+
+        private float _jumpTimeoutDelta;
+        private float _fallTimeoutDelta;
+
+        private int _animIDSpeed;
+        private int _animIDGrounded;
+        private int _animIDJump;
+        private int _animIDFreeFall;
+        private int _animIDMotionSpeed;
+
+#if ENABLE_INPUT_SYSTEM
+        private PlayerInput _playerInput;
+#endif
+        private Animator _animator;
+        private CharacterController _controller;
+        private StarterAssetsInputs _input;
+        private GameObject _mainCamera;
+
+        private const float _threshold = 0.01f;
+        private bool _hasAnimator;
+
+        private bool isGliding = false;
+        private float trailFadeTimer = 0f;
+
+        private bool IsCurrentDeviceMouse =>
+#if ENABLE_INPUT_SYSTEM
+            _playerInput.currentControlScheme == "KeyboardMouse";
+#else
+            false;
+#endif
+
+        private void Awake()
+        {
+            if (_mainCamera == null)
+                _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+
+        private void Start()
+        {
+            _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
+            _hasAnimator = TryGetComponent(out _animator);
+            _controller = GetComponent<CharacterController>();
+            _input = GetComponent<StarterAssetsInputs>();
+#if ENABLE_INPUT_SYSTEM
+            _playerInput = GetComponent<PlayerInput>();
+#endif
+            AssignAnimationIDs();
+            _jumpTimeoutDelta = JumpTimeout;
+            _fallTimeoutDelta = FallTimeout;
+        }
+
+        private void Update()
+        {
+            _hasAnimator = TryGetComponent(out _animator);
+            HandleJumpInput();
+            JumpAndGravity();
+            GroundedCheck();
+            Move();
+            HandleVeilEffect();
+        }
+
+        private void LateUpdate() => CameraRotation();
+
+        private void AssignAnimationIDs()
+        {
+            _animIDSpeed = Animator.StringToHash("Speed");
+            _animIDGrounded = Animator.StringToHash("Grounded");
+            _animIDJump = Animator.StringToHash("Jump");
+            _animIDFreeFall = Animator.StringToHash("FreeFall");
+            _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+        }
+
+        private void HandleJumpInput()
+        {
+            if (_input.jump)
+            {
+                if (Grounded && _jumpTimeoutDelta <= 0.0f)
+                {
+                    _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+                    if (_hasAnimator) _animator.SetBool(_animIDJump, true);
+                    _input.jump = false;
+                }
+                else if (!Grounded && veilCharge >= glideCost)
+                {
+                    StartGlide();
+                    _input.jump = false;
+                }
+            }
+        }
+
+        private void StartGlide()
+        {
+            _verticalVelocity = glideLiftForce;
+            isGliding = true;
+            veilCharge -= glideCost;
+            trailFadeTimer = 0.5f;
+
+            if (VeilEffect != null)
+                VeilEffect.emitting = true;
+        }
+
+        private void JumpAndGravity()
+        {
+            if (Grounded)
+            {
+                isGliding = false;
+                _fallTimeoutDelta = FallTimeout;
+
+                if (_hasAnimator)
+                {
+                    _animator.SetBool(_animIDJump, false);
+                    _animator.SetBool(_animIDFreeFall, false);
+                }
+
+                if (_verticalVelocity < 0.0f)
+                    _verticalVelocity = -2f;
+
+                _jumpTimeoutDelta -= Time.deltaTime;
+            }
+            else
+            {
+                _jumpTimeoutDelta = JumpTimeout;
+
+                if (_fallTimeoutDelta > 0.0f)
+                    _fallTimeoutDelta -= Time.deltaTime;
+                else if (_hasAnimator)
+                    _animator.SetBool(_animIDFreeFall, true);
+
+                if (isGliding)
+                    _verticalVelocity += Gravity * glideGravityMultiplier * Time.deltaTime;
+                else
+                    _verticalVelocity += Gravity * Time.deltaTime;
+            }
+
+            _verticalVelocity = Mathf.Clamp(_verticalVelocity, -_terminalVelocity, float.MaxValue);
+            _controller.Move(Vector3.up * _verticalVelocity * Time.deltaTime);
+        }
+
+        private void HandleVeilEffect()
+        {
+            if (Grounded)
+            {
+                if (VeilEffect != null)
+                    VeilEffect.emitting = false;
+
+                trailFadeTimer = 0f;
+            }
+            else
+            {
+                if (trailFadeTimer > 0f)
+                    trailFadeTimer -= Time.deltaTime;
+
+                if (VeilEffect != null)
+                    VeilEffect.emitting = isGliding || trailFadeTimer > 0f;
+            }
+        }
+
+        private void GroundedCheck()
+        {
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
+            Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
+            if (_hasAnimator) _animator.SetBool(_animIDGrounded, Grounded);
+        }
+
+        private void CameraRotation()
+        {
+            if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+            {
+                _cinemachineTargetYaw += _input.look.x * Time.deltaTime;
+                _cinemachineTargetPitch += _input.look.y * Time.deltaTime;
+            }
+
+            _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+            CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
+        }
+
+        private static float ClampAngle(float angle, float min, float max)
+        {
+            if (angle < -360f) angle += 360f;
+            if (angle > 360f) angle -= 360f;
+            return Mathf.Clamp(angle, min, max);
+        }
+
+        private void Move()
+        {
+            float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+
+            float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+            _speed = Mathf.Lerp(_speed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
+
+            Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+            if (_input.move != Vector2.zero)
+            {
+                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
+
+            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+            _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime));
+
+            if (_hasAnimator)
+            {
+                _animator.SetFloat(_animIDSpeed, _speed);
+                _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+            }
+        }
+
+        public void RechargeVeil(float amount)
+        {
+            veilCharge = Mathf.Clamp(veilCharge + amount, 0f, maxVeilCharge);
+        }
+
+        public void OnLand() => Debug.Log("Landed");
+        public void OnFootstep() => Debug.Log("Footstep");
+    }
+}
